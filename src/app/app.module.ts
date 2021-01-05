5c58f18d6e6c3a36cbde3aa1df982f885a2305df
alzca1 <alzca1@gmail.com>
2021-01-05 17:48:31 +0100

    added home, ships, signin and signup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { ShipsComponent } from './ships/ships.component';
+import { ShipComponent } from './ships/ship/ship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent,
+    ShipsComponent,
+    ShipComponent
   ],
   imports: [
     BrowserModule
