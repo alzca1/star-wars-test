44d96ce23bf461950e6dea387642d807a7ed3ad2
alzca1 <alzca1@gmail.com>
2021-01-07 11:58:20 +0100

    added sign-in component html structure and form setup

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  signInForm: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.signInForm = new FormGroup({
+      userData: new FormGroup({
+        email: new FormControl(null, Validators.required),
+        password: new FormControl(null, Validators.required),
+      }),
+    });
   }
-
 }
